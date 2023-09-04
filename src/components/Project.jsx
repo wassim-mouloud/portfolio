17b8d92b2e928f5cc0f42a79c7cdc6d329013e02
wassim-mouloud <simo.rko10@gmail.com>
2023-09-04 13:58:13 +0200

    adding videos

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -49,13 +49,13 @@ function Project({name, description, img, vid, projectLink}) {
         return (
             <motion.div  initial='hidden' whileInView='show' variants={container} viewport={{once:true}} className='flex flex-col items-center justify-center gap-10 px-5 mt-6 mb-5 md:flex-row rounded-xl' >
                 <motion.div onMouseEnter={()=>onMouseEnter()} onMouseLeave={()=>onMouseLeave()} variants={appear} viewport={{once:true}} className='relative order-2 md:order-1'>
-                                {/* <video 
+                                <video 
                                     src={`/videos/${vid}.mp4`}  
                                     autoPlay={true}
                                     muted 
                                     loop 
                                     className='md:w-[400px] lg:w-[550px] absolute inset-0 opacity-0 hover:opacity-100 transition-opacity duration-500 object-contain rounded-xl'
-                                /> */}
+                                />
                                 <motion.img  src={`/images/${img}.png`} alt="" className='w-[90vw] md:w-[400px] lg:w-[550px]   object-contain rounded-xl'/>
                 </motion.div>
                 <motion.div variants={appear} viewport={{once:true}} className='flex flex-col justify-center gap-5 md:w-[400px] order-1 md:order-2' >
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -5,7 +5,7 @@ function Projects() {
   return (
     <div id='projects' className='flex flex-col items-center px-4' >
         <h1 className='text-[32px] lg:text-[44px] mb-[80px] font-bold text-white' >My Projects</h1>
-        <Project name='1. Disney Plus Clone' description='A disney plus clone made using the TMDB API .' img='disney' vid='disney' projectLink='https://disneyplus-f1110.web.app/' />
+        <Project name='1. Disney Plus Clone' description='A disney plus clone made using the TMDB API .' img='disney' vid='disney' projectLink='https://disneyplus-f1110.web.app/'  />
         <Project name='2. Metaversus' description="Metaverse is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real ." img='metaversus' vid='metaversus' projectLink='https://metaversus-sd2j.vercel.app/'/>
         <Project name='3. TennisFam' description='A tennis website built with ReactJS , it gives you all the information about my favourite sport which is obviously tennis .' img='tennis' vid='tennis' projectLink='https://tennis-f70a4.web.app/' />
 
