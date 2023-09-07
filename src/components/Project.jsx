c6b638c2db7d672af104f82d53ee03ce1872c9bd
wassim-mouloud <simo.rko10@gmail.com>
2023-09-07 22:12:22 +0200

    making skills as an argument

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 import {motion} from 'framer-motion'
 import '../App.css'
 
-function Project({name, description, img, vid, projectLink}) {
+function Project({name, description, img, vid, projectLink, skills}) {
 
     const [hovered, setHovered]= useState(false)
+    const {s1, s2, s3} = skills
+    
     const onMouseEnter = ()=>{
         setHovered(true)
     }
@@ -29,23 +31,7 @@ function Project({name, description, img, vid, projectLink}) {
                 }
         }
       }
-    // if(window.innerWidth<768){
-    //     return (
-    //         <motion.div initial='hidden' whileInView='show' variants={container} viewport={{once:true}} className='flex flex-col gap-10 mt-6 md:flex-row' >
-    //             <motion.div variants={appear} viewport={{once:true}} className='flex flex-col gap-5 md:w-[400px]' >
-    //                 <h2 className='font-semibold text-white text-[24px]' >{name}</h2>
-    //                 <div className='flex gap-3' >
-    //                     <img src="/images/react-logo.png" alt="" className='w-6 h-6'/>
-    //                     <img src="/images/tailwind-logo.png" alt="" className='w-6 h-6'/>
-    //                     <img src="/images/firebase-logo.png" alt="" className='w-6 h-6'/>
-    //                 </div>
-    //                 <p className='text-white max-w-[90vw]' >{description}</p>
-    //                 <a href={projectLink} target='_blank'><button className='group flex items-center justify-center h-9  border border-white  rounded-md bg-black  w-[220px] p-3  transition-all duration-300 ' ><p className='font-semibold text-white ' >Visit website</p></button></a>
-    //             </motion.div>
-    //             <motion.img variants={appear} viewport={{once:true}} src={`/images/${img}.png`} alt="" className='w-[90vw] md:w-[550px]  object-contain rounded-xl'/>
-    //     </motion.div>
-    //     )
-    // }else{
+
         return (
             <motion.div  initial='hidden' whileInView='show' variants={container} viewport={{once:true}} className='flex flex-col items-center justify-center gap-10 px-5 mt-6 mb-5 md:flex-row rounded-xl' >
                 <motion.div onMouseEnter={()=>onMouseEnter()} onMouseLeave={()=>onMouseLeave()} variants={appear} viewport={{once:true}} className='relative order-2 md:order-1'>
@@ -61,9 +47,9 @@ function Project({name, description, img, vid, projectLink}) {
                 <motion.div variants={appear} viewport={{once:true}} className='flex flex-col justify-center gap-5 md:w-[400px] order-1 md:order-2' >
                     <h2 className='font-semibold text-white md:text-[26px] lg:text-[32px]' >{name}</h2>
                     <div className='flex gap-3' >
-                        <img src="/images/react-logo.png" alt="" className='w-8 h-8'/>
-                        <img src="/images/tailwind-logo.png" alt="" className='w-8 h-8'/>
-                        <img src="/images/firebase-logo.png" alt="" className='w-8 h-8'/>
+                        <img src={`/images/${s1}-logo.png`} alt="" className='w-8 h-8'/>
+                        <img src={`/images/${s2}-logo.png`} alt="" className='w-8 h-8'/>
+                        {s3 && (<img src={`/images/${s3}-logo.png`} alt="" className='w-8 h-8'/>)}
                     </div>
                     <p className='text-white font-normal max-w-[90vw] md:text-[16px] lg:text-[18px]' >{description}</p>
                     <a href={projectLink} target='_blank'><button className='group flex items-center justify-center h-9  border border-white  rounded-md bg-[#0d0d0d]  w-[220px] p-3  transition-all duration-300 btn' ><p className='font-semibold text-white transition-all duration-300 group-hover:text-[#0d0d0d] z-[99]' >Visit website</p></button></a>
